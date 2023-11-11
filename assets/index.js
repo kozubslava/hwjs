8db29679a90701dd3f5e9e95f36c740a9b9d838a
Вячеслав <146669463+kozubslava@users.noreply.github.com>
2023-11-12 00:08:27 +0200

    hw calculation

---
+++ assets/index.js
@@ -0,0 +1,38 @@
+let num1 = 100;
+let num2 = 25; 
+let result1 = num1 + num2
+console.log(result1);
+
+let result2 = result1 - 50;
+console.log(result2);
+
+let result3 = result1 * result2
+console.log(result3);
+
+let result4 = result3 / 2.687
+console.log(result4);
+
+let remainder1 = result3 % 2.687
+console.log(remainder1);
+
+console.log(result1 > 1);
+console.log(!!result1);
+
+
+let num = 5;
+
+// num = num +10;
+num += 10
+
+// num = num * 2;
+num *= 2;
+
+// num = num - 4;
+num -= 4;
+
+// num = num / 6;
+num /= 6;
+console.log(num);
+
+
+
